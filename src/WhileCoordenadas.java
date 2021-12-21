f3209b03c8e3a66c60594000600b0c348375ee41
Lucas <lucas_borba@outlook.com>
2021-12-21 15:53:22 -0300

    WhileCoordenadas

---
+++ src/WhileCoordenadas.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class WhileCoordenadas {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        while (x != 0 && y != 0) {
+            if (x > 0 && y > 0) {
+                System.out.println("Primeiro");
+            } else if (x < 0 && y > 0) {
+                System.out.println("Segundo");
+            } else if (x < 0 && y < 0) {
+                System.out.println("Terceiro");
+            } else {
+                System.out.println("Quarto");
+            }
+            x = sc.nextInt();
+            y = sc.nextInt();
+        }
+        sc.close();
+    }
+}
+
+/*programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema
+cartesiano. Para cada ponto escrever o quadrante a que ele pertence. O algoritmo será encerrado quando pelo
+menos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem alguma)*/
