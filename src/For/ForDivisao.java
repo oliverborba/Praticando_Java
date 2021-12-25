dfcb211c0c30b9e515fe7632c325ae348fb5668b
Lucas <lucas_borba@outlook.com>
2021-12-25 12:23:27 -0300

    Laço For

--- src/For/ForDivisao.java
+++ src/For/ForDivisao.java
@@ -21,3 +21,5 @@ public class ForDivisao {
         sc.close();
     }
 }
+/*Fazer um programa para ler um número N. Depois leia N pares de números e mostre a divisão do primeiro pelo
+segundo. Se o denominador for igual a zero, mostrar a mensagem "divisao impossivel"*/
